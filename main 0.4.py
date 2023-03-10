d265f3a2cb82e7b1706377c8eb84d3c26bedf402
Xx-Nasank-xX <125129998+Xx-Nasank-xX@users.noreply.github.com>
2023-03-10 18:00:20 +0100

    Update main 0.4.py

--- main 0.4.py
+++ main 0.4.py
@@ -1,7 +1,7 @@
-import pygame 
+import pygame ,numpy
+
 vec = pygame.math.Vector2
 class Jeu :
-    
     def __init__(self) -> None:
         self.width , self.height = 720,480
         self.screen = pygame.display.set_mode((self.width,self.height))
@@ -17,59 +17,70 @@ class Jeu :
 class Entity:
     def __init__(self,screen,blockliste,entite) -> None:
         self.screen = screen
+        self.rect = pygame.Rect(50,50,32,64)
         self.blockliste = blockliste
         self.decalage = 0
+        self.speed = 4
         self.entite = entite
         self.joueur = False
-        self.pos = vec((10, 360))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        self.FRIC = -0.12
-        self.ACC = 0.5
         if self.entite == 0:
             self.joueur = True
+            self.pos = vec((10, 360))
             self.rect = pygame.Rect(50,50,32,64)
             self.image = pygame.transform.scale(pygame.image.load('assets/Mario.png'),(32,64))
             
         else:
-            self.rect = pygame.Rect(50,50,32,32)
-            self.image = pygame.transform.scale(pygame.image.load('assets/brique.png'),(32,32))
-            self.ACC -= 0.25
+            self.pos = vec((150,450))
+            self.rect = pygame.Rect(50,50,32,64)
+            self.image = pygame.transform.scale(pygame.image.load('assets/Luigi.png'),(32,64))
+            self.speed = 3
         
+        self.pos = vec((200, 0))
+        self.jumpspeed = 20
+        self.speedVerti = 0
+        self.speedHori = 0
+        self.gravity = 1
+        self.min_jumpspeed = 4
+        self.prev_key = pygame.key.get_pressed()
+
     def draw(self):
         self.rect.x -= self.decalage
         self.screen.blit(self.image,self.rect)
 
-    def collisionDown(self):
-        for e in self.blockliste.values():
-            for i in e :
-                if pygame.Rect.colliderect(self.rect, i) == True:
-                    return (True,i)
-        return (False,0)
-
     def jump(self):
-        if self.joueur == True:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                if self.collisionDown()[0]:
-                    self.vel.y = -15
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.vel.y = -15
 
-    def move(self):
-        self.acc = vec(0,0.5)
-        if self.joueur == True:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_RIGHT]:
-                self.acc.x = self.ACC
-            if keys[pygame.K_LEFT]:
-                self.acc.x = -self.ACC
-        else:
-            self.acc.x = self.ACC
-            
-        self.acc.x += self.vel.x * self.FRIC
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+    def updateTuto(self):
+        self.speedHori = 0
+        onground = self.check_collision(0, 1)
+        # check keys
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.speedHori = -self.speed
+        elif key[pygame.K_RIGHT]:
+            self.speedHori = self.speed
 
-        self.rect.midbottom = self.pos
+        if key[pygame.K_UP] and onground:
+            self.speedVerti = -self.jumpspeed
+
+        # variable height jumping
+        if self.prev_key[pygame.K_UP] and not key[pygame.K_UP]:
+            if self.speedVerti < -self.min_jumpspeed:
+                self.speedVerti = -self.min_jumpspeed
+
+        self.prev_key = key
+
+        # gravity
+        if self.speedVerti < 10 and not onground:  # 9.8 rounded up
+            self.speedVerti += self.gravity
+
+        if onground and self.speedVerti > 0:
+            self.speedVerti = 0
+
+        # movement
+        self.move(self.speedHori, self.speedVerti)
 
     def inputs(self):
         for event in pygame.event.get():
@@ -77,18 +88,36 @@ class Entity:
                 pygame.quit()
                 exit()
 
-    def gravity(self):
-        if self.vel.y > 0:
-            if self.collisionDown()[0]:
-                self.vel.y = 0
-                self.pos.y = self.collisionDown()[1].top + 1
+    def check_collision(self, x, y):
+        collide = False
+        self.pos += [x,y]
+        self.rect.midbottom = self.pos
+        for e in self.blockliste.values():
+            for i in e :
+                if pygame.Rect.colliderect(self.rect, i) == True:
+                    collide = True
+        self.pos += [-x,-y]
+        self.rect.midbottom = self.pos
+        return collide
+    
+    def move(self,x,y):
+        dx = x
+        dy = y
+        
+        while self.check_collision(0, dy):
+            dy -= numpy.sign(dy)
+
+        while self.check_collision(dx, dy):
+            dx -= numpy.sign(dx)
+        
+        self.pos += [dx,dy]
+
+        self.rect.midbottom = self.pos
 
     def update(self):
-        self.draw()
-        self.move()
         self.inputs()
-        self.jump()
-        self.gravity()
+        self.updateTuto()
+        self.draw()
 
 class World:
     def __init__(self,screen) -> None:
@@ -98,7 +127,6 @@ class World:
         self.blockSize = 32
         self.decalage = 0
         self.briqueimg = pygame.transform.scale(pygame.image.load('assets/brique.png'),(32,32))
-
 
         self.liste = []
         self.elementIntoListe()
@@ -114,7 +142,7 @@ class World:
 
         self.mario = Entity(self.screen,self.blockliste,0)
         self.monstre = Entity(self.screen,self.blockliste,1)
-
+        
     def get_world(self):
         file = open('world.txt', 'r')
         data = file.read()
@@ -139,14 +167,22 @@ class World:
                 self.screen.blit(self.briqueimg,rect)
             elif e[2] == '?':
                 rect = pygame.Rect(e[1]*self.blockSize-self.decalage, e[0]*self.blockSize, self.blockSize, self.blockSize)
-                pygame.draw.rect(self.screen, "yellow", rect ,2)   
-
+                pygame.draw.rect(self.screen, "yellow", rect ,2)
+                
+    
+    def recharge(self):
+        if self.mario.rect.x < 0 and self.mario.rect.y > 1500 :
+            self.mario.pos = vec((200, 0))
 
     def scrolling(self):
-        if self.mario.rect.x-self.decalage > self.width*0.75:
-            self.decalage += self.mario.vel[0]
-            self.mario.decalage += self.mario.vel[0]
-            self.monstre.decalage += self.mario.vel[0]
+        
+        if self.mario.rect.x-self.mario.decalage >= self.width*0.75-self.decalage:     
+            self.decalage += self.mario.speedHori
+            self.mario.decalage = self.decalage
+
+        if self.mario.rect.x-self.mario.decalage <= self.width*0.25-self.decalage and self.decalage>0:     
+            self.decalage += self.mario.speedHori
+            self.mario.decalage = self.decalage
 
     def inputs(self):
         keys = pygame.key.get_pressed()
@@ -161,9 +197,9 @@ class World:
 
     def update(self):
         self.inputs()
+        self.recharge()
         self.scrolling()
         self.draw_on_screen()
-        self.monstre.update()
         self.mario.update()
         
 
@@ -172,3 +208,5 @@ class World:
 jeu = Jeu()
 while True :
     jeu.update()
+
+
